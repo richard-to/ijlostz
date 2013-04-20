ae115fe5eb5d2f8407b07e052178a4119f2b05a4
richard-to <gohn76@gmail.com>
2013-04-20 03:25:39 -0800

    Add test for tetris controls

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@
     var EL = "body";
 
     var canvas = document.getElementById(CANVAS_ID);
-    var tetris = new Tetris.Game(EL, new Tetris.CanvasView(canvas));
-    tetris.run();
+
+    var shapes = [Tetris.ShapeS, Tetris.ShapeO, Tetris.ShapeL];
+    var shapeBag = new TetrisGA.MockGenerator(shapes);
+
+    var tetris = new Tetris.Game(
+        EL, new Tetris.CanvasView(canvas), shapeBag, {keysEnabled: false});
+
+    var Control = Tetris.Control;
+    var moves = [
+        Control.LEFT,
+        Control.LEFT,
+        Control.LEFT,
+        Control.HARDDROP,
+        Control.RIGHT,
+        Control.RIGHT,
+        Control.RIGHT,
+        Control.HARDDROP
+    ];
+
+    var movePlayer = new TetrisGA.MovePlayer(tetris, moves, 150);
+    movePlayer.play();
 })();
