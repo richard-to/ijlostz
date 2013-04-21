956469f4a782eec25ce4ca62677ac185b6eb0eb0
richard-to <gohn76@gmail.com>
2013-04-20 16:32:08 -0800

    Add rough implementation of genetype sequencer

--- js/lib/ijlostz.ga.js
+++ js/lib/ijlostz.ga.js
@@ -26,7 +26,7 @@
 
     MockGenerator.prototype.nextShape = function() {
         var shape = this.bag.shift();
-        return new shape();
+        return shape;
     };
     TetrisGA.MockGenerator = MockGenerator;
 
@@ -53,6 +53,83 @@
         }
     };
     TetrisGA.MovePlayer = MovePlayer;
+
+    var Control = Tetris.Control;
+    var GenotypeToMoveSequencer = {
+        sequence: function(genotype, shapes) {
+            var moves = [];
+            var coordX = genotype.coordX;
+            var rotation = genotype.rotation;
+            var length = coordX.length;
+            for (var i = 0; i < length; i++) {
+                if (rotation[i] == 1) {
+                    moves.push(Control.ROTATE_RIGHT);
+                } else if (rotation[i] == 2) {
+                    moves.push(Control.ROTATE_RIGHT);
+                    moves.push(Control.ROTATE_RIGHT);
+                } else if (rotation[i] == 3) {
+                    moves.push(Control.ROTATE_LEFT);
+                }
+                if (coordX[i] < shapes[i].start.x) {
+                    var shapeIndex = rotation[i] % shapes[i].shape.length;
+                    var shape = shapes[i].shape[shapeIndex];
+                    var moves1 = shapes[i].start.x;
+                    while(moves1 > coordX[i]) {
+                        moves.push(Control.LEFT);
+                        moves1--;
+                    }
+
+                    var xSpace = 0;
+                    var found = false;
+                    for (var x = 0; x < shape[0].length; x++) {
+                        for (var y = 0; y < shape.length; y++) {
+                            if (shape[y][x] > 0) {
+                                xSpace = x;
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found)
+                            break;
+                    }
+
+                    while (xSpace > 0) {
+                        moves.push(Control.LEFT);
+                        xSpace--;
+                    }
+                } else if (coordX[i] > shapes[i].start.x) {
+                    var shapeIndex = rotation[i] % shapes[i].shape.length;
+                    var shape = shapes[i].shape[shapeIndex];
+                    var moves1 = shapes[i].start.x;
+                    while(moves1 < coordX[i]) {
+                        moves.push(Control.RIGHT);
+                        moves1++;
+                    }
+
+                    var xSpace = 0;
+                    var found = false;
+                    for (var x = shape[0].length - 1; x >= 0; x--) {
+                        for (var y = 0; y < shape.length; y++) {
+                            if (shape[y][x] > 0) {
+                                xSpace = x;
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (found)
+                            break;
+                    }
+                    while (xSpace < shape[0].length - 1) {
+                        moves.push(Control.LEFT);
+                        xSpace++;
+                    }
+                }
+                moves.push(Control.HARDDROP);
+            }
+            return moves;
+        }
+    };
+    TetrisGA.GenotypeToMoveSequencer = GenotypeToMoveSequencer;
 
     window.TetrisGA = TetrisGA;
 })(window);
