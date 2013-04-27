ac03f0156046adc7b77e4055c371ef4e26409804
richard-to <gohn76@gmail.com>
2013-04-26 16:52:28 -0800

    Add test main

--- js/main.js
+++ js/main.js
@@ -4,16 +4,36 @@
 
     var canvas = document.getElementById(CANVAS_ID);
 
-    var shapes = [Tetris.ShapeS, Tetris.ShapeO, Tetris.ShapeL];
-    var shapeBag = new TetrisGA.MockGenerator(shapes);
+    var randomGen = new Tetris.RandomGenerator(Tetris.ShapeList);
+    var shapes = TetrisGA.initializeShapes(10, randomGen);
 
-    var sequence = TetrisGA.initializeGenePool(1, 3);
-    var moves = TetrisGA.convertGenotypeToMoves(sequence[0], Tetris.ShapeList);
+    var sequence = TetrisGA.initializeGenePool(6, shapes.length);
 
-    var tetris = new Tetris.Game(
-        EL, new Tetris.CanvasView(canvas), shapeBag, {keysEnabled: true});
+    window.simulate = simulate;
+    window.simulate(sequence, 0, shapes);
 
-    var player = new TetrisGA.ComputerPlayer(tetris, moves, 200);
-    player.play();
-
+    var generations = 4;
+    var current = 0;
+    function simulate(sequence, index, shapes) {
+        var moves = TetrisGA.convertGenotypeToMoves(sequence[index], shapes);
+        var shapeBag = new TetrisGA.MockGenerator(_.clone(shapes));
+        var tetris = new Tetris.Game(
+            EL, new TetrisGA.CanvasView(canvas), shapeBag, {keysEnabled: false});
+        var self = this;
+        var player = new TetrisGA.ComputerPlayer(tetris, moves, 200, function(score) {
+            sequence[index].fitness = score;
+            index++;
+            if (index < sequence.length) {
+                self.simulate(sequence, index, shapes);
+            } else {
+                var parents = TetrisGA.tournamentSelection(sequence);
+                var children = TetrisGA.crossoverNPoint(parents, 2, .9);
+                var mutations = TetrisGA.mutationRandomReset(children, 0.1);
+                current++;
+                if (current < 4)
+                    self.simulate(sequence, 0, shapes);
+            }
+        });
+        player.play();
+    }
 })();
